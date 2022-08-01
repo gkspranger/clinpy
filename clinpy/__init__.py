76e7a59e69025ad2ad2c6caebf6a8220cc060cc5
gkspranger <greg@spranger.us>
2022-08-01 17:02:25 -0400

    closer

--- clinpy/__init__.py
+++ clinpy/__init__.py
@@ -1,9 +1,27 @@
-def let(bindings: list, *exprs):
+from itertools import zip_longest
+from functools import reduce, partial
+
+def _chunk_bindings(bindings):
+    return list(
+        zip_longest(*[iter(bindings)] * 2)
+    )
+
+def _arg_val(state, arg):
+    return state[arg] if arg in state.keys() else arg
+
+def _eval_binding(state, binding):
+    key = binding[0]
+    form = binding[1]
+    if isinstance(form, tuple):
+        symbol = form[0]
+        args = map(partial(_arg_val, state), form[1:])
+        state[key] = symbol(*args)
+    else:
+        state[key] = form
+    return state
+
+def let(bindings, *exprs):
     """Evaluates the exprs in a lexical context in which the symbols in
     the binding-forms are bound to their respective init-exprs or parts
     therein."""
-    is_callable_list = lambda x: isinstance(x, list | tuple) and callable(x[0])
-
-    for pair in zip(*([iter(bindings)] * 2)):
-        print(pair)
-    return 1
+    return reduce(lambda acc, binding: _eval_binding(acc, binding), _chunk_bindings(bindings), {})
--- tests/clinpy/test_init.py
+++ tests/clinpy/test_init.py
@@ -6,8 +6,10 @@ def add(x, y): return x + y
 def test_let():
     assert \
         clinpy.let(
-            ("let:a", sum([1,2,3]),
-            "let:b", (pow, "let:a", 2),
-            "let:c", (add, "let:a", "let:b")),
-            (factorial, "let:c")
+            (
+                "let:a", sum([1,2,3]),
+                "let:b", (pow, "let:a", 2),
+                "let:c", (add, "let:a", "let:b")
+            ),
+            (print, "let:c")
         ) == 1
